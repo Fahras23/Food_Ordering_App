749c56822d61edd1b13232b5cf52df6ba558f132
Fahras23 <2001krzysztoflyczak@gmail.com>
2023-06-13 00:06:45 +0200

    add truted origins

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -60,6 +60,8 @@ MIDDLEWARE = [
     "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
+CSRF_TRUSTED_ORIGINS = ['web-production-2e4f.up.railway.app']
+
 ROOT_URLCONF = 'foodapp.urls'
 
 TEMPLATES = [
